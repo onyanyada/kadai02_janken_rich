6e698593e9ce15e9d4264c084035c1ce13cbeaca
uetani <shiiizuka.furuta@gmail.com>
2024-08-07 23:14:29 +0900

    ガチャアイテムのハズレの場合の挙動を追加

--- js/gacha-use.js
+++ js/gacha-use.js
@@ -4,9 +4,25 @@ $('.gachaUse').click(function () {
         $('.gacha_option_btn').css('display', 'block');
     }
 
-    if (gachaItemGet === "鍵") {
+    else if (gachaItemGet === "鍵") {
         $('.openLodTotal').css('display', 'block');
         $('.openLodTotal').html(`<div>ロッドの合計好感度は${totalL}点</div>`);
+    }
+
+    else if (gachaItemGet === "はずれ") {
+        $('.openLodTotal').css('display', 'block');
+        $('.openLodTotal').text("アイテムは使えません");
+
+    }
+
+    else if (gachaItemGet === "魔法を使える猫") {
+        // totalfunc = () => {
+        //     // 王子のトータル計算
+        //     totalPrince = score1Prince + score2Prince + score3Prince + score4Prince + score5Prince;
+        //     // ロッドのトータル計算
+        //     totalL = score1L + score2L + score3L + score4L + score5L + 1;
+        // }
+
     }
 
 });
