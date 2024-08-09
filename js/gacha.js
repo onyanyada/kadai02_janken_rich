db0d7d751d75d7cbd0e4b10b9164eca8cdb89012
uetani <shiiizuka.furuta@gmail.com>
2024-08-09 23:43:10 +0900

    ガチャのじゃんけんのチョキ、パーの選択肢の意味がなかったので消去

--- js/gacha.js
+++ js/gacha.js
@@ -25,7 +25,7 @@ $(".gu_btn6").on("click", function () {
     if (r == 3) {
         gachaImg = '<img src="./img/maho.png" width="100px">';
         gachaItem = '魔法を使える猫';
-        gachaExp = '最初に使うとロッドの好感度を1上げることができるよ';
+        gachaExp = 'ロッドの好感度を1上げることができるよ';
         gachaScore = 2;
     }
     if (r == 4) {
@@ -35,81 +35,6 @@ $(".gu_btn6").on("click", function () {
         gachaScore = 2;
     }
 
-
-
-    //3.表示処理
-    resultView6();
-
-
-});
-
-// チョキを選んだ時
-$(".cho_btn6").on("click", function () {
-    const r = Math.ceil(Math.random() * 4);
-
-    //2.if分岐処理
-    if (r == 1) {
-        gachaImg = '<img src="./img/key.png" width="100px">';
-        gachaItem = '鍵';
-        gachaExp = 'ロッドの合計好感度を一度だけ確認できるよ';
-        gachaScore = -1;
-    }
-    if (r == 2) {
-        gachaImg = '<img src="./img/cha.png" width="100px">';
-        gachaItem = '異国の茶葉';
-        gachaExp = 'お茶会で使うことができるよ';
-        gachaScore = 1;
-    }
-    if (r == 3) {
-        gachaImg = '<img src="./img/maho.png" width="100px">';
-        gachaItem = '魔法を使える猫';
-        gachaExp = '最初に使うとロッドの好感度を1上げることができるよ';
-        gachaScore = 1;
-
-    }
-    if (r == 4) {
-        gachaImg = '<img src="./img/null.png" width="100px">';
-        gachaItem = 'はずれ';
-        gachaExp = 'ポイ活したらガチャにまた挑戦できるよ';
-        gachaScore = 2;
-    }
-
-
-
-    //3.表示処理
-    resultView6();
-
-
-});
-
-// パーを選んだ時
-$(".par_btn6").on("click", function () {
-    const r = Math.ceil(Math.random() * 4);
-    //2.if分岐処理
-    if (r == 1) {
-        gachaImg = '<img src="./img/key.png" width="100px">';
-        gachaItem = '鍵';
-        gachaExp = 'ロッドの合計好感度を一度だけ確認できるよ';
-        gachaScore = -1;
-    }
-    if (r == 2) {
-        gachaImg = '<img src="./img/cha.png" width="100px">';
-        gachaItem = '異国の茶葉';
-        gachaExp = 'お茶会で使うことができるよ';
-        gachaScore = 1;
-    }
-    if (r == 3) {
-        gachaImg = '<img src="./img/maho.png" width="100px">';
-        gachaItem = '魔法を使える猫';
-        gachaExp = '最初に使うとロッドの好感度を1上げることができるよ';
-        gachaScore = 2;
-    }
-    if (r == 4) {
-        gachaImg = '<img src="./img/null.png" width="100px">';
-        gachaItem = 'はずれ';
-        gachaExp = 'ポイ活したらガチャにまた挑戦できるよ';
-        gachaScore = 2;
-    }
 
 
     //3.表示処理
