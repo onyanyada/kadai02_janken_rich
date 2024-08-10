0759a82f54706b9e4881197e7f83dc37887c2dce
uetani <shiiizuka.furuta@gmail.com>
2024-08-10 22:15:02 +0900

    #nextBtnがクリックするたびに次のシナリオの番号に変わるようにした

--- js/script.js
+++ js/script.js
@@ -21,6 +21,8 @@ if (randomEvent == 0) {
 }
 
 const n = scenarios.length;//ループ5回実行確定させる
+// まず、vを格納する配列を作成
+const vArray = [];
 for (let i = 0; i < n; i++) {
     // ランダムな数字を定数ranに入れる(配列の数の範囲で)
     const ran = Math.floor(Math.random() * scenarios.length);//0~4
@@ -30,6 +32,9 @@ for (let i = 0; i < n; i++) {
     // spliceはfruitsからran番目の物を削除するので重複を避けられる
     const v = scenarios.splice(ran, 1);
 
+    vArray.push(v); // vを配列に追加
+
+
     // scenarioというクラス名をもつdivを取り出し配列にする
     const scenarioArray = Array.from(document.getElementsByClassName("scenario"));
     // console.log(Array.isArray(scenarioArray));// 配列かチェック
@@ -37,15 +42,30 @@ for (let i = 0; i < n; i++) {
 
     // scenariosから取り出したvの名前をscenarioArrayの中身と形式を合わせる
     const classShow = `div.${v}.scenario`;
-    // console.log(classShow);//div.u.scenarioなど1つずつループで回されて5つ出る
+    console.log(classShow);//div.s1.scenarioなど1つずつループで回されて5つ出る
 
     // classShowに名前で一致するscenarioArrayの中身をmatchesメソッドで見つける
     const result = scenarioArray.find(item => item.matches(classShow));
-    // console.log(result); //<div class="u scenario">u</div>がランダム順に5つ出る
+    //console.log(result); //<div class="s1 scenario">中身</div>がランダム順に5つ出る
 
     // <div class="u scenario">u</div>などを.showのhtmlに追加しdisplay:block;にする
     $('.show').append(result).children().addClass('mieru');//
+
 }
+console.log(vArray);
+// 現在のリンク番号を追跡するためのインデックスを定義
+// vArray[1]から順に使いたいので初期値を1にしている
+// 最初のvArray[0]はすでに表示されている
+let currentIndex = 1;
+
+$('#nextScBtn').click(function () {
+    // vArrayから順番にvを取得
+    if (currentIndex < vArray.length) {
+        $('#nextScBtn').find('a').attr('href', `#c${vArray[currentIndex]}`);
+        //↑vArray[1]→vArray[2]…の順番で使う
+        currentIndex++;//次のクリック時には次のvを使う
+    }
+});
 
 
 // ガチャをストーリーに反映
